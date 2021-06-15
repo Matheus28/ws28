8be17562524b7612f37b01f89f9caf0cd190d517
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-06-15 07:42:00 -0400

    Added sha1::toHexString back

--- src/sha1.cpp
+++ src/sha1.cpp
@@ -11,5 +11,16 @@ namespace sha1 {
         SHA1_Final(hash, &sha1);
 
     }
+    void toHexString(const unsigned char* hash, char* hexstring)
+    {
+        const char hexDigits[] = { "0123456789abcdef" };
+
+        for (int hashByte = 20; --hashByte >= 0;)
+        {
+            hexstring[hashByte << 1] = hexDigits[(hash[hashByte] >> 4) & 0xf];
+            hexstring[(hashByte << 1) + 1] = hexDigits[hash[hashByte] & 0xf];
+        }
+        hexstring[40] = 0;
+    }
 }
 }
